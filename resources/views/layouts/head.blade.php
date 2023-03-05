b162dc1c5a2a8fa1a355346e1533db29b0a4ead4
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-05 09:05:39 -0500

    add icon to title

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -2,6 +2,6 @@
     <meta charset="utf-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
-    <title>Registro : : usuarios</title>    
-    <img src="" alt="">
+    <title>SuperPc : : Usuarios</title>    
+    <link rel="shortcut icon" href="{{ asset('img/icon.png')}}">
 </head>
