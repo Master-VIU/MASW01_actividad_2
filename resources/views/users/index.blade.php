4b16fbf803004253ea7587f7347057a667c67787
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-06 17:26:49 -0500

    Table is added to the index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,34 @@
 @include('../layouts.head')
 
-<h1>Listado de usuarios registrados</h1>
- 
-@foreach ($data as $item => $values)
-    <ul>
-        <li>{{$values}}</li>
-        
-    </ul>
-@endforeach
+<div class="container-table">
+
+    <h1>Listado de usuarios registrados<img src="{{ asset('img/icon.png') }}" width="120"></h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Nombre completo</th>
+                <th scope="col">Dni</th>
+                <th scope="col">País</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        @foreach ($data as $item)
+            <tbody>
+                <tr>
+                    <td>{{ $item->name }} {{ $item->surnames }}</td>
+                    <td>{{ $item->dni }}</td>
+                    <td>{{ $item->country }}</td>
+                    <td>
+                        <button type="submit" class="btn btn-primary">Editar</button>
+                        <button type="submit" class="btn btn-danger">Borrar</button>
+                        </form>
+                    </td>
+                </tr>
+            </tbody>
+        @endforeach
+    </table>
+</div>
+
 
 @include('../layouts.footer')
