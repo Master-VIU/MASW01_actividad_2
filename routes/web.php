26575713b8761cddf353e3ab9506c14808bd435a
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-06 22:42:36 -0500

    Add routers

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+/*Route::controller(UserController::class)->group(function() {
+    Route::get('/index', 'index');
+    Route::get('/registro', 'create');
+    Route::post('/store', 'store');
+});*/
 Route::get('/registro', [UserController::class, 'create'])->name('users.create');
 Route::post('/store', [UserController::class, 'store'])->name('users.store');
 Route::get('/index', [UserController::class, 'index'])->name('users.index');
+Route::delete('/index/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::get('/{id}/editar', [UserController::class, 'edit'])->name('users.edit');
+Route::put('/{id}/update', [UserController::class, 'update'])->name('users.update');
+    
