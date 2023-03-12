8b1bc94d01973ac20c3f4213b92f3a4d0ce13b5a
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-11 21:06:01 -0500

    The name of the user_id column is changed to id, since the authentication generated an error

--- database/migrations/2023_02_28_230359_create_users_table.php
+++ database/migrations/2023_02_28_230359_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('user_id');
+            $table->id('id');
             $table->string('name', 20)->nullable(false);
             $table->string('surnames', 40)->nullable(false);
             $table->string('dni', 9)->nullable(false);
