724f3daa15d3eb1a308c7e0e4b5535615b7ae37d
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-11 21:02:49 -0500

    Logo is added in the h1

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,64 +1,67 @@
+<html>
 @section('title')
     REGISTRO
 @endsection
 @include('../layouts.head')
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Por favor corregir los errores:</strong>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
 
-<h1>Registro de usuarios <img src="{{ asset('img/icon.png')}}" width="150"></h1>
+<body>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Por favor corregir los errores:</strong>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <h1>Registro de usuarios <img src="{{ asset('img/icon.png') }}" width="150"></h1>
 
     <div class="container">
         <form action="{{ route('users.store') }}" method="post">
-            @csrf           
+            @csrf
             <div class="col-md-4">
-                <label for="Nombre" class="form-label">Nombre:</label><br>
-                <input type="text" class="form-control" name="nombre" id="nombre" minlength="2" maxlength="20"
+                <label for="nombre" class="form-label">Nombre:</label><br>
+                <input type="text" class="form-control" name="nombre" id="nombre" autofocus minlength="2" maxlength="20"
                     value="{{ old('nombre') }}">
             </div>
             <div class="col-md-4">
-                <label for="Apellidos" class="form-label">Apellidos:</label><br>
+                <label for="apellidos" class="form-label">Apellidos:</label><br>
                 <input type="text" class="form-control"name="apellidos" id="apellidos" minlength="2" maxlength="40"
                     value="{{ old('apellidos') }}">
             </div>
 
             <div class="col-md-4">
-                <label for="Dni" class="form-label">Dni:</label><br>
+                <label for="dni" class="form-label">Dni:</label><br>
                 <input type="text" class="form-control"name="dni" id="dni" minlength="9" maxlength="9"
                     value="{{ old('dni') }}">
             </div>
 
             <div class="col-md-4">
                 <label for="email" class="form-label">Email:</label><br>
-                <input type="email" class="form-control"name="email" id="email" value="{{ old('email') }}">
+                <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}">
             </div>
 
             <div class="col-md-4">
-                <label for="Password" class="form-label">Contraseña:</label>
+                <label for="password" class="form-label">Contraseña:</label>
                 <input type="password" class="form-control"name="contraseña" id="contraseña" required>
             </div>
 
             <div class="col-md-4">
-                <label for="Password" class="form-label">Confirmar contraseña:</label>
+                <label for="password" class="form-label">Confirmar contraseña:</label>
                 <input type="password" class="form-control"name="confirmarContraseña" id="confirmarContraseña" required>
             </div>
 
             <div class="col-md-4">
-                <label for="Telefono" class="form-label">Teléfono:</label>
+                <label for="telefono" class="form-label">Teléfono:</label>
                 <input type="tel" class="form-control"name="telefono" id="telefono" minlength="9" maxlength="12"
                     pattern="[+]{1}[0-9]{11}" value="{{ old('telefono') }}">
             </div>
 
             <div class="col-md-4">
-                <label for="Pais" class="form-label">País:</label>
-                <input type="text" class="form-control"name="pais" id="pais" minlength="9"
+                <label for="pais" class="form-label">País:</label>
+                <input type="text" class="form-control"name="pais" id="pais" minlength="3"
                     value="{{ old('pais') }}">
             </div>
 
@@ -76,7 +79,7 @@
             <div class="col-12">
                 <button type="submit" class="btn btn-primary">Guardar</button>
                 <a href="{{ route('users.index') }}">
-                    <input type="button" class="btn btn-secondary" value="Volver al login">
+                    <input type="button" class="btn btn-secondary" value="Volver">
                 </a>
             </div>
 
@@ -84,3 +87,8 @@
     </div>
 
     @include('../layouts.footer')
+
+
+</body>
+
+</html>
