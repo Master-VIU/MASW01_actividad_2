81a4c36c3ad6d8f2c8fd09fc3ab2fc09c35fea90
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-12 21:04:15 -0500

    Code is formatted and error block is eliminated

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,92 +1,83 @@
 <html>
-@section('title')
-    REGISTRO
-@endsection
+
+@include('../layouts.errors')
 @include('../layouts.head')
 
-<body>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Por favor corregir los errores:</strong>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+<h1>Registro de usuarios <img src="{{ asset('img/icon.png') }}" width="150"></h1>
+
+<div class="container">
+    <form action="{{ route('users.store') }}" method="post">
+        @csrf
+        <div class="col-md-4">
+            <label for="nombre" class="form-label">Nombre:</label><br>
+            <input type="text" class="form-control" name="nombre" id="nombre" autofocus minlength="2"
+                maxlength="20" value="{{ old('nombre') }}" placeholder="nombre">
         </div>
-    @endif
+        <div class="col-md-4">
+            <label for="apellidos" class="form-label">Apellidos:</label><br>
+            <input type="text" class="form-control"name="apellidos" id="apellidos" minlength="2" maxlength="40"
+                value="{{ old('apellidos') }}" placeholder="apellidos">
+        </div>
 
-    <h1>Registro de usuarios <img src="{{ asset('img/icon.png') }}" width="150"></h1>
+        <div class="col-md-4">
+            <label for="dni" class="form-label">Dni:</label><br>
+            <input type="text" class="form-control"name="dni" id="dni" minlength="9" maxlength="9"
+                value="{{ old('dni') }}" placeholder="dni">
+        </div>
 
-    <div class="container">
-        <form action="{{ route('users.store') }}" method="post">
-            @csrf
-            <div class="col-md-4">
-                <label for="nombre" class="form-label">Nombre:</label><br>
-                <input type="text" class="form-control" name="nombre" id="nombre" autofocus minlength="2" maxlength="20"
-                    value="{{ old('nombre') }}">
-            </div>
-            <div class="col-md-4">
-                <label for="apellidos" class="form-label">Apellidos:</label><br>
-                <input type="text" class="form-control"name="apellidos" id="apellidos" minlength="2" maxlength="40"
-                    value="{{ old('apellidos') }}">
-            </div>
+        <div class="col-md-4">
+            <label for="email" class="form-label">Email:</label><br>
+            <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}"
+                placeholder="email@email.com">
+        </div>
 
-            <div class="col-md-4">
-                <label for="dni" class="form-label">Dni:</label><br>
-                <input type="text" class="form-control"name="dni" id="dni" minlength="9" maxlength="9"
-                    value="{{ old('dni') }}">
-            </div>
+        <div class="col-md-4">
+            <label for="password" class="form-label">Contraseña:</label>
+            <input type="password" class="form-control"name="contraseña" id="contraseña" placeholder="contraseña"
+                required>
+        </div>
 
-            <div class="col-md-4">
-                <label for="email" class="form-label">Email:</label><br>
-                <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}">
-            </div>
+        <div class="col-md-4">
+            <label for="password" class="form-label">Confirmar contraseña:</label>
+            <input type="password" class="form-control"name="confirmarContraseña" id="confirmarContraseña"
+                placeholder="confirmar contraseña" required>
+        </div>
 
-            <div class="col-md-4">
-                <label for="password" class="form-label">Contraseña:</label>
-                <input type="password" class="form-control"name="contraseña" id="contraseña" required>
-            </div>
+        <div class="col-md-4">
+            <label for="telefono" class="form-label">Teléfono:</label>
+            <input type="tel" class="form-control"name="telefono" id="telefono" minlength="9" maxlength="12"
+                pattern="[+]{1}[0-9]{11}" value="{{ old('telefono') }}" placeholder="teléfono">
+        </div>
 
-            <div class="col-md-4">
-                <label for="password" class="form-label">Confirmar contraseña:</label>
-                <input type="password" class="form-control"name="confirmarContraseña" id="confirmarContraseña" required>
-            </div>
+        <div class="col-md-4">
+            <label for="pais" class="form-label">País:</label>
+            <input type="text" class="form-control"name="pais" id="pais" minlength="3"
+                value="{{ old('pais') }}" placeholder="país">
+        </div>
 
-            <div class="col-md-4">
-                <label for="telefono" class="form-label">Teléfono:</label>
-                <input type="tel" class="form-control"name="telefono" id="telefono" minlength="9" maxlength="12"
-                    pattern="[+]{1}[0-9]{11}" value="{{ old('telefono') }}">
-            </div>
+        <div class="col-md-8">
+            <label for="iban" class="form-label">Número cuenta bancaria (IBAN):</label>
+            <input type="text" class="form-control"name="iban" id="iban" required value="{{ old('iban') }}"
+                placeholder="IBAN">
+        </div>
 
-            <div class="col-md-4">
-                <label for="pais" class="form-label">País:</label>
-                <input type="text" class="form-control"name="pais" id="pais" minlength="3"
-                    value="{{ old('pais') }}">
-            </div>
+        <div class="col-md-10">
+            <label for="sobreTi">Sobre ti:</label>
+            <input class="form-control" name="sobreTi" id="sobreTi" minlength="20" maxlength="250"
+                value="{{ old('sobreTi') }}" placeholder="Sobre ti">
+        </div>
+        <br>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Guardar</button>
+            <a href="{{ route('users.index') }}">
+                <input type="button" class="btn btn-secondary" value="Volver">
+            </a>
+        </div>
 
-            <div class="col-md-8">
-                <label for="iban" class="form-label">Número cuenta bancaria (IBAN):</label>
-                <input type="text" class="form-control"name="iban" id="iban" required
-                    value="{{ old('iban') }}">
-            </div>
+    </form>
+</div>
 
-            <div class="col-md-10">
-                <label for="sobreTi">Sobre ti:</label>
-                <textarea class="form-control" name="sobreTi" id="sobreTi" minlength="20" maxlength="250"></textarea>
-            </div>
-            <br>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary">Guardar</button>
-                <a href="{{ route('users.index') }}">
-                    <input type="button" class="btn btn-secondary" value="Volver">
-                </a>
-            </div>
-
-        </form>
-    </div>
-
-    @include('../layouts.footer')
+@include('../layouts.footer')
 
 
 </body>
