b968d079409e76f51249c4ea56ff76d897c3acb0
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-12 21:03:17 -0500

    The block that walks through the list of errors is removed

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,18 +1,9 @@
 <html>
+
+@include('../layouts.errors')
 @include('../layouts.head')
 
 <body>
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Por favor corregir los errores:</strong>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 
     <h1>Editar usuario <img src="{{ asset('img/icon.png') }}" width="150"></h1>
     <div class="container">
@@ -20,49 +11,50 @@
             @csrf
             {{ method_field('PUT') }}
             <div class="col-md-4">
-                <label for="Nombre" class="form-label">Nombre:</label><br>
+                <label for="nombre" class="form-label">Nombre:</label><br>
                 <input type="text" class="form-control" name="nombre" id="nombre" minlength="2" maxlength="20"
-                    value="{{ $user->name }}">
+                    value="{{ $user->name }}" placeholder="nombre">
             </div>
             <div class="col-md-4">
-                <label for="Apellidos" class="form-label">Apellidos:</label><br>
+                <label for="apellidos" class="form-label">Apellidos:</label><br>
                 <input type="text" class="form-control"name="apellidos" id="apellidos" minlength="2" maxlength="40"
-                    value="{{ $user->surnames }}">
+                    value="{{ $user->surnames }}" placeholder="apellidos">
             </div>
 
             <div class="col-md-4">
-                <label for="Dni" class="form-label">Dni:</label><br>
+                <label for="dni" class="form-label">Dni:</label><br>
                 <input type="text" class="form-control"name="dni" id="dni" minlength="9" maxlength="9"
-                    value="{{ $user->dni }}">
+                    value="{{ $user->dni }}" placeholder="dni">
             </div>
 
             <div class="col-md-4">
                 <label for="email" class="form-label">Email:</label><br>
-                <input type="email" class="form-control"name="email" id="email" value="{{ $user->email }}">
+                <input type="email" class="form-control"name="email" id="email" value="{{ $user->email }}"
+                    placeholder="email@email.com">
             </div>
 
             <div class="col-md-4">
-                <label for="Telefono" class="form-label">Teléfono:</label>
+                <label for="telefono" class="form-label">Teléfono:</label>
                 <input type="tel" class="form-control"name="telefono" id="telefono" minlength="9" maxlength="12"
-                    pattern="[+]{1}[0-9]{11}" value="{{ $user->phone }}">
+                    pattern="[+]{1}[0-9]{11}" value="{{ $user->phone }}" placeholder="teléfono">
             </div>
 
             <div class="col-md-4">
-                <label for="Pais" class="form-label">País:</label>
+                <label for="pais" class="form-label">País:</label>
                 <input type="text" class="form-control"name="pais" id="pais" minlength="3"
-                    value="{{ $user->country }}">
+                    value="{{ $user->country }}" placeholder="país">
             </div>
 
             <div class="col-md-8">
                 <label for="iban" class="form-label">Número cuenta bancaria (IBAN):</label>
                 <input type="text" class="form-control"name="iban" id="iban" required
-                    value="{{ $user->iban }}">
+                    value="{{ $user->iban }}" placeholder="IBAN">
             </div>
 
             <div class="col-md-10">
                 <label for="sobreTi">Sobre ti:</label>
-                <textarea class="form-control" name="sobreTi" id="sobreTi" minlength="20" maxlength="250"
-                    value="{{ $user->over_you }}"></textarea>
+                <input class="form-control" name="sobreTi" id="sobreTi" minlength="20" maxlength="250"
+                    value="{{ $user->over_you }}" placeholder="Sobre ti">
             </div>
             <br>
             <div class="col-12">
