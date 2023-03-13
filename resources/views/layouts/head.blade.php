6239ef901fd7bf1378325400546bb2dad8748ce0
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-12 21:04:53 -0500

    Code is formatted and the logout option is changed from anchor to button

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,28 +1,29 @@
 <head>
     <meta charset="utf-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
-    <title>SuperPc : : Usuarios</title>    
-    <link rel="shortcut icon" href="{{ asset('img/icon.png')}}">
+    <title>SuperPc : : Usuarios</title>
+    <link rel="shortcut icon" href="{{ asset('img/icon.png') }}">
 
-    
-@if (Session::has('success'))
-<div class="alert alert-success">
-    <p class="">{{ Session::get('success') }} </p>
-</div>
-@elseif(Session::has('danger'))
-<div class="alert alert-danger">
-    <p class="">{{ Session::get('danger') }} </p>
-</div>
-@endif
+
+    @if (Session::has('success'))
+        <div class="alert alert-success">
+            <p class="">{{ Session::get('success') }} </p>
+        </div>
+    @elseif(Session::has('danger'))
+        <div class="alert alert-danger">
+            <p class="">{{ Session::get('danger') }} </p>
+        </div>
+    @endif
 </head>
 
 @auth
-<nav class="logout">
-    <a href="{{ route('users.logout') }}">
-       Cerrar sesión
-    </a>
-</nav>
-    
-@endauth
+    <nav class="logout">
+        <form action="{{ url('users/logout') }}" method="POST">
+            @csrf
+            <button class="btn btn-outline-secondary" type="submit"> Cerrar sesión </button>
+        </form>
+    </nav>
 
+@endauth
