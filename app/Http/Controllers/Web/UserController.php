011ce06d595d8e461a9431e7ea2e3f44786e0680
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-15 17:44:43 -0500

    Web folder is changed

---
+++ app/Http/Controllers/Web/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Web\RequestEditValidate;
+use App\Models\User;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\View;
+
+class UserController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $users = User::all();
+        $data = json_decode($users);
+
+        return view('users.index', ['data' => $data]);
+
+    }
+
+       /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return View::make('users.edit')->with('user', $user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(RequestEditValidate $request, $id)
+    {
+        $request->validated();
+
+        $user = User::findOrFail($id);
+        $user->name = strtoupper($request->nombre);
+        $user->surnames = strtoupper($request->apellidos);
+        $user->dni = strtoupper($request->dni);
+        $user->email = $request->email;
+        $user->phone = $request->telefono;
+        $user->country = $request->pais;
+        $user->iban = strtoupper($request->iban);
+        $user->over_you = $request->sobreTi;
+        $changes = $user->getDirty();
+        $user->save();
+        if ($changes) {
+            return Redirect::to('users/index')->with('success', 'El usuario ha sido modificado correctamente.');
+        }
+        return Redirect::to('users/index')->with('danger', 'No se han actualizado datos.');
+
+
+    }
+
+}
