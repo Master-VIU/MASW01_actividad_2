6765c1b3e98d9f066766876e11d520f8db5cb803
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-15 22:18:07 -0500

    route correction

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthControllerApi;
+use App\Http\Controllers\Api\UserControllerApi;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,12 +17,21 @@ use App\Http\Controllers\Api\AuthControllerApi;
 
 const AUTH = 'auth:sanctum';
 const GUEST = 'guest';
+const MESSAGE = 'message';
+const CODE = 'code';
+const DATA = 'data';
 
 Route::middleware(GUEST)->controller(AuthControllerApi::class)->group (function () {
     Route::post('register','register');
     Route::post('login', 'login');
 });
 
-Route::middleware(AUTH)->controller(AuthControllerApi::class)->group (function () {
+// Route::middleware(AUTH)->controller(AuthControllerApi::class)->group (function () {
+//     Route::get('users', 'usersList');
+// });
+
+Route::middleware(AUTH)->controller(UserControllerApi::class)->group (function () {
     Route::get('users', 'usersList');
+    Route::put('/{id}', 'update');
+    Route::post('/{id}', 'destroy');
 });
