f984419bca30efb4a079b3f05704e2b19e141b5b
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-15 22:19:32 -0500

    Controller is created for the API

---
+++ app/Http/Controllers/Api/UserControllerApi.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\RequestEditValidate;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+class UserControllerApi extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function usersList(Request $request)
+    {
+
+        $user = User::all();
+        $totalUsers = $user->count();
+        return response()->json([CODE => RouteServiceProvider::OK, 'total_users' => $totalUsers, DATA => $user], Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(RequestEditValidate $request, $id)
+    {
+        try {
+            $request->validated();
+
+            $user = User::findOrFail($id);
+            $user->name = strtoupper($request->nombre);
+            $user->surnames = strtoupper($request->apellidos);
+            $user->dni = strtoupper($request->dni);
+            $user->email = $request->email;
+            $user->phone = $request->telefono;
+            $user->country = $request->pais;
+            $user->iban = strtoupper($request->iban);
+            $user->over_you = $request->sobreTi;
+            $user->createToken('auth_token')->plainTextToken;
+            $changes = $user->getDirty();
+            $user->save();
+            if ($changes) {
+                return response()->json([
+                    CODE => RouteServiceProvider::OK,
+                    MESSAGE => "Usuario actualizado con exito",
+                    DATA => $user
+                ], Response::HTTP_OK);
+            }
+
+        } catch (Throwable $e) {
+            return response()->json([
+                CODE => RouteServiceProvider::ERROR,
+                MESSAGE => 'Error, al intentar actualizar el usuario'
+            ], Response::HTTP_OK);
+        }
+
+        return response()->json([CODE => RouteServiceProvider::INFO, MESSAGE => 'No se realizaron cambios en el usuario'], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return response()->json([
+            CODE => RouteServiceProvider::OK,
+            MESSAGE => 'Usuario eliminado con exito!'
+        ], Response::HTTP_OK);
+    }
+}
