ee1535e33f9da091598e3c4102bce7015f6c3e68
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-18 12:41:18 -0500

    Validations and method that generates the messages are added

--- app/Http/Requests/Api/RequestEditValidate.php
+++ app/Http/Requests/Api/RequestEditValidate.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Requests\Api;
 
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class RequestEditValidate extends FormRequest
 {
@@ -15,13 +19,21 @@ class RequestEditValidate extends FormRequest
     public function rules()
     {
         return [
-            'nombre' => 'required|min:2|max:20',
+            'nombre' => 'required|min:2|max:20|regex:/^[\pL\s]+$/u',
             'apellidos' => 'required|min:2|max:40|regex:/^[\pL\s]+$/u',
             'dni' => 'required|max:9|regex:/^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKET]$/i',
-            'telefono' => 'min:9|max:12|nullable',
+            'telefono' => 'min:9|max:12|nullable|regex:/^[+]{1}[0-9,+]*$/',
             'pais' => 'alpha|nullable',
             'iban' => 'alpha_num|min:24|max:30|required|regex:/^[A-Za-z]{2}[0-9]{22}+$/u',
             'sobreTi' => 'min:20|max:250|nullable|regex:/^[A-Za-z0-9,.]+/',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            RouteServiceProvider::CODE => '422',
+            RouteServiceProvider::MESSAGE =>$validator->errors(),
+        ], Response::HTTP_UNPROCESSABLE_ENTITY));
     }
 }
